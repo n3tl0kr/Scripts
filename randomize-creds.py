ea06623b190c4e7166998e43a12274d14bff3dc5
Paul Goffar <n3tl0kr@ravencybersec.com>
2025-01-21 00:50:11 -0500

    Optimized randomize script

--- randomize-creds.py
+++ randomize-creds.py
@@ -15,25 +15,21 @@ parser.add_argument("--passwords", "-p", type=str, required=True, help="Flat tex
 parser.add_argument("--encode", "-e", action='store_true', help="Output as Base64 encoded values")
 args = parser.parse_args()
 
-# import users
-users = open(args.users, 'r')
-x = users.readlines()
-users.close()
+# Read users and passwords
+with open(args.users, 'r') as users_file:
+    users = users_file.read().splitlines()
 
-# import passwords
-passwds = open(args.passwords, 'r')
-y = passwds.readlines()
-passwds.close()
+with open(args.passwords, 'r') as passwds_file:
+    passwords = passwds_file.read().splitlines()
 
-if args.encode:
-    output = [[base64.b64encode(bytes(user.rstrip('\n') + ':' + (passwd.rstrip('\n'))))]
-              for user in x
-              for passwd in y if user != passwd]
+# Generate and print output
+for user in users:
+    for passwd in passwords:
+        if user != passwd:
+            credential = f"{user}:{passwd}"
+            if args.encode:
+                encoded = base64.b64encode(credential.encode('utf-8')).decode('utf-8')
+                print(encoded)
+            else:
+                print(credential)
 
-else:
-    output = [[user.rstrip('\n') + ':' + passwd.rstrip('\n')]
-              for user in x
-              for passwd in y if user != passwd]
-
-for row in output:
-    print(" ".join(map(str, row)))
